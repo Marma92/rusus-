467bab057169fc6432077aa1d55f2988be26f222
Amram ELBAZ <elbaz.amram@gmail.com>
2024-01-21 10:31:49 +0100

    now a really really nice sky

---
+++ webpack.config.cjs
@@ -0,0 +1,18 @@
+// webpack.config.js
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'public'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+    ],
+  },
+};
